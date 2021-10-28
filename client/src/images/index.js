b8b57d4ff390f7ed3f1883667554677cb7042eac
Debasis Pattanaik <67583843+debasis-pattanaik-au16@users.noreply.github.com>
2021-10-28 15:43:14 +0530

    Add files via upload

---
+++ client/src/images/index.js
@@ -0,0 +1,35 @@
+import AUTHOR_1 from "./author-1.jpg";
+import AUTHOR_2 from "./author-2.jpg";
+import AUTHOR_3 from "./author-3.jpg";
+import AUTHOR_4 from "./author-4.jpg";
+import BALOON from "./baloon.svg";
+import CALL_ACTION from "./call-to-action.png";
+import CLIENT_LOGO_1 from "./client_logo_01.png";
+import CLIENT_LOGO_2 from "./client_logo_02.png";
+import CLIENT_LOGO_3 from "./client_logo_03.png";
+import CLIENT_LOGO_4 from "./client_logo_04.png";
+import FAVICON from "./favicon.png";
+import FLOWER from "./flower.svg";
+import HEADER_BACKGROUND from "./header-bg.jpg";
+import HEADER_SHAPE from "./header-shape.svg";
+import LOGO from "./logo.svg";
+import SERVICES from "./services.png";
+
+export {
+  AUTHOR_1,
+  AUTHOR_2,
+  AUTHOR_3,
+  AUTHOR_4,
+  BALOON,
+  CALL_ACTION,
+  CLIENT_LOGO_1,
+  CLIENT_LOGO_2,
+  CLIENT_LOGO_3,
+  CLIENT_LOGO_4,
+  FAVICON,
+  FLOWER,
+  HEADER_BACKGROUND,
+  HEADER_SHAPE,
+  LOGO,
+  SERVICES,
+};
