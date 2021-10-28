031c01bdde4c3a8dc10fd335c40fb56424e3d094
Debasis Pattanaik <67583843+debasis-pattanaik-au16@users.noreply.github.com>
2021-10-28 15:42:07 +0530

    Add files via upload

---
+++ client/src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./css/bootstrap.min.css";
+import "./css/default.css";
+import "./css/LineIcons.css";
+import "./css/magnific-popup.css";
+import "./css/slick.css";
+import "./css/style.css";
+import App from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
